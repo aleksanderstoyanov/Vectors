afb6b3589a4b6d0b81f8f0daa0880a30e8ccc7a9
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-05-29 15:57:57 +0300

    Adding main functionality for menus

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
-#include "Element.h"
+#include "Menu.h"
+#include "MenuFile.h"
+#include "Tetrahedron.h"
+#include "TestLine.h"
+#include "TestTriangle.h"
+
 using namespace std;
 
 int main() {
+
+	cout << "Choose between options 1 or 2" << endl;
+	cout << "1.Menu without file" << endl << "2.Menu with file" << endl;
+	int option;
+	cin >> option;
+	while (true)
+	{
+		if (option == 1 || option == 2)
+		{
+			break;
+		}
+		cout << "Not a valid option try again !" << endl;
+		cin >> option;
+	}
+	try
+	{
+
+		switch (option)
+		{
+		case 1:
+			printMenu();
+			break;
+		case 2:
+			printMenuFile();
+			break;
+
+		}
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what();
+	}
+
+
+
 
 }
