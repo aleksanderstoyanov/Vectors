0eacafd0fda1f72424950f2880d8b2ab919ef1c9
Borislav Rapondjiev <bobsa290@gmail.com>
2021-05-29 14:11:16 +0300

    Adding menu functionality

---
+++ Vectors/Menu.h
@@ -0,0 +1,11 @@
+#ifndef Menu
+#define Menu
+#include <iostream>
+int printMenu();
+int printAllPointOperations();
+int printAllVectorOperations();
+int printAllLineOperations();
+int printAllSegmentOperations();
+int printAllTriangleOperations();
+
+#endif 
